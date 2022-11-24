fd1144b06907e2e36d003da505af21a15f62a60c
info-mic-info <jobsocial94@gmail.com>
2022-11-24 17:00:06 +0100

    Complete

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@
 // 3- Dare la possibilità all'utente di inserire il suo colore preferito 
 // 4- Concatenare i valori delle variabili concatenando anche il numero 21 alla fine della stringa
 // 5- Visualizzare a schermo all'interno della pagina
+
+
+
+// 1- Dare la possibilità all'utente di inserire il nome 
+let your_name = prompt ("Inserisci il tuo nome")
+
+// 2- Dare la possibilità all'utente di inserire il cognone
+let your_surname = prompt ("Inserisci il tuo cognome")
+
+// 3- Dare la possibilità all'utente di inserire il suo colore preferito 
+let your_color = prompt ("Inserisci il tuo colore preferito")
+
+// 4- Concatenare i valori delle variabili concatenando anche il numero 21 alla fine della stringa
+document.getElementById("password").innerHTML= your_name + your_surname + your_color + "21";
+
+// 5- Visualizzare a schermo all'interno della pagina
+console.log(your_name + your_surname + your_color + "21");
