c0a269ce86f089365643e43c5553d3ddeff2bfbe
info-mic-info <jobsocial94@gmail.com>
2022-11-24 16:29:08 +0100

    Lista cose da fare

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// PROBLEMA: Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito Infine scrivi sulla pagina nomecognomecolorepreferito21.
+
+
+
+// 1- Dare la possibilità all'utente di inserire il nome 
+// 2- Dare la possibilità all'utente di inserire il cognone
+// 3- Dare la possibilità all'utente di inserire il suo colore preferito 
+// 4- Concatenare i valori delle variabili concatenando anche il numero 21 alla fine della stringa
+// 5- Visualizzare a schermo all'interno della pagina
